2609b194fb38ff48661f93a3bfb284b4088f0baf
Niaomi James <catlover1996309@gmail.com>
2022-03-22 10:05:16 -0400

    chapter 11

---
+++ api/db.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+const { MongoClient } = require('mongodb');
+const url = process.env.DB_URL || 'mongodb://127.0.0.1/cop2836';
+let db;
+
+async function connectToDb() {
+  const client = new MongoClient(url, { useNewUrlParser: true });
+  await client.connect();
+  console.log('Connected to MongoDB at', url);
+  db = client.db();
+}
+async function getNextSequence(name) {
+  const result = await db.collection('counters').findOneAndUpdate(
+    { _id: name },
+    { $inc: { current: 1 } },
+    { returnOriginal: false },
+  );
+  return result.value.current;
+}
+
+function getDb(){
+  return db;
+}
+
+module.exports = { connectToDb, getNextSequence, getDb };
--- api/scripts/init.mongo.js
+++ api/scripts/init.mongo.js
@@ -1,16 +1,38 @@
 
+/* global db print */
+/* eslint no-restricted-globals: "off"  */
+
 db.issues.remove({});
+db.deleted_issues.remove({});
 
 const issuesDB = [
   {
-    id: 1, status: "New", owner: "Vitalis", effort: 5,
-   created: new Date ('2022-01-18'), due: undefined,
-   title:"No1 Climatechange"
+   id: 1,
+   status: "New",
+   owner: "Vitalis",
+   effort: 5,
+   created: new Date ('2022-01-18'),
+   due: undefined,
+   title:"No1 Climatechange",
+   description: "Steps to prevent Climate change : "
+   + '\n1. Limit the amount of Pollution.'
+   + '\n2. Change the way we use water consumption.'
+   + '\n3. Use energy saving for power usages.'
+   + '\n3. Plant more trees.',
    },
    {
-   id: 2, status: "Assigned", owner: "Mmamotse", effort: 14,
-   created: new Date ('2021-01-17'), due: new Date('2021-01-25'),
-   title:"  No2 Diversity"
+   id: 2,
+   status: "Assigned",
+   owner: "Mmamotse",
+   effort: 14,
+   created: new Date ('2021-01-17'),
+   due: new Date('2021-01-25'),
+   title:"  No2 Diversity",
+   description: "Steps to reconstruct Diversity: "
+   + '\n1. unused property or industrial land,be reconstructed into a safe place for wildlife.'
+   + '\n2. Stop hunting in areas that dont need it.'
+   + '\n3. Make sure invasive species are at a low population, to help native animals recoperate.',
+
    },
 ];
 
@@ -25,3 +47,4 @@ db.issues.createIndex({ id: 1 }, { unique: true });
 db.issues.createIndex({ status: 1});
 db.issues.createIndex({ owner: 1});
 db.issues.createIndex({ created: 1});
+db.deleted_issues.createIndex({ id: 1 }, { unique: true });
